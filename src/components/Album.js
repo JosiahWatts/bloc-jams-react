fd1305cea2bb21ab5af7c5cfae2e532f4f50ecec
Josiah Watts <josiah.c.watts@gmail.com>
2018-05-06 14:02:11 -0400

    Checkpoint 4: complete

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Album = () => (
-  <section className="album">
-    Albums Page will go here!
-  </section>
-);
+class Album extends React.Component {
+  render(){
+    return (
+      <section className="album">
+        {this.props.match.params.slug} Album will go here
+      </section>
+    );
+  }
+}
 
 export default Album;
